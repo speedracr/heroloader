488f11433839d10f0498afa23bd5b958d689f5d1
Jonas Knipper <jonas@robocomobo.com>
2016-01-16 19:23:19 +0100

    Set up JS for upload form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,21 @@
 //= require turbolinks
 //= require jquery.ui.widget
 //= require jquery.fileupload
+
+$(function() {
+  $('.directUpload').find("input:file").each(function(i, elem) {
+    var fileInput = $(elem);
+    console.log(fileInput);
+  });
+});
+
+$(function() {
+  $('.directUpload').find("input:file").each(function(i, elem) {
+    var fileInput     = $(elem);
+    var form          = $(fileInput.parents('form:first'));
+    var submitButton  = form.find('input[type="submit"]');
+    var progressBar   = $("<div class='bar'></div>");
+    var barContainer  = $("<div class='progress'></div>").append(progressBar);
+    fileInput.after(barContainer);
+  });
+});
